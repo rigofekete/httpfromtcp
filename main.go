5acbee5ba5527497719a8c44a896ead92676a617
rigofekete <fabricecosteira@gmail.com>
2026-01-21 19:55:55 +0100

    created getLinesChannel func. refactored main to call it

--- main.go
+++ main.go
@@ -16,33 +16,48 @@ func main() {
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
 	}
-	defer file.Close()
 
-	currentLine := ""
-	for {
-		buffer := make([]byte, 8, 8)
-		n, err := file.Read(buffer) 
-		if err != nil {
-			if currentLine != "" {
-				fmt.Printf("read: %s\n", currentLine)
-				currentLine = ""
-			}
-			if errors.Is(err, io.EOF) {
-				break
-			}
-			fmt.Printf("error: %s\n", err.Error())
-			break
-		}
-
-		str := string(buffer[:n])
-		parts := strings.Split(str, "\n")
-		for i := 0; i < len(parts)-1; i++ {
-			fmt.Printf("read: %s%s\n", currentLine, parts[i])
-			currentLine = ""
-		}
-		currentLine += parts[len(parts)-1]
+	// os.File implements io.ReadCloser interface, we can use it as arg
+	chStrings := getLinesChannel(file)
+	for line := range chStrings {
+		fmt.Printf("read: %s\n", line)
 	}
+
 }
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	chStrings := make(chan string)
 
+	go func() {
+		defer f.Close()
+		defer close(chStrings)
+
+		currentLine := ""
+		for {
+			buffer := make([]byte, 8, 8)
+			n, err := f.Read(buffer) 
+			if err != nil {
+				if currentLine != "" {
+					chStrings <- currentLine
+					currentLine = ""
+				}
+				if errors.Is(err, io.EOF) {
+					return 
+				}
+				fmt.Printf("error: %s\n", err.Error())
+				return
+			}
+
+			str := string(buffer[:n])
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				chStrings <- currentLine + parts[i]
+				currentLine = ""
+			}
+			currentLine += parts[len(parts)-1]
+		}
+	}()
+
+	return chStrings
+}
 
